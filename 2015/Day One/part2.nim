d240a4ea1debfcb6593289145cd498c2ffec7e57
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-07 19:05:42 +0700

    update part
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ 2015/Day One/part2.nim
@@ -0,0 +1,19 @@
+import strutils
+
+let content:string = readFile("input.txt").strip()
+var
+  num:int = 0
+  res: seq[int]
+
+for i in 0..content.len - 1:
+  if num == -1:
+    res.add(i)
+  case content[i]:
+  of '(':
+    num += 1
+  of ')':
+    num -= 1
+  else:
+    break
+
+echo res[0]
